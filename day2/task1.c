b371dc91d8902d31a16131a78e6742e300d998ac
keyurgohil13 <gohilkeyur13@gmail.com>
2023-07-19 13:19:35 +0530

    Day2 Lecture.

---
+++ day2/task1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+main(){
+	
+	int a = 10;
+	int b = 20;
+	
+	printf("Ans == %d", a+b);
+	
+}
---
+++ day2/task2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main(){
+	
+	int r;
+	float area;
+	const pi = 3.14;
+	
+	area = pi*r*r;
+	
+	printf("Area of Circle == %.2f", area);
+	
+	
+}s
